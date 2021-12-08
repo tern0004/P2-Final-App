c582950598bdccd03ea46916a740fb2c48853a7b
Paul Rubchinski <rubchinskipaul@gmail.com>
2021-12-08 15:55:33 -0500

    - Initial setup

---
+++ screens/ExpenseDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+function ExpenseDetails() {
+    return (
+        <View>
+            <Text>This is Expense Details Screen!</Text>
+        </View>
+    );
+}
+
+export default ExpenseDetails;
