43302080296132206922d867a9a4296ac63d9b7e
Paul Rubchinski <rubchinskipaul@gmail.com>
2021-12-09 10:39:04 -0500

    - Initial setup (Fixed)

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { useState, useEfect } from 'react';
+import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from "./screens/HomeScreen";
+
+import HomeScreen from './screens/HomeScreen';
 import ExpenseDetails from "./screens/ExpenseDetails";
 
-const Stack = createStackNavigator();
-export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Expense Details" component={ExpenseDetails} />
-        </Stack.Navigator>
-      </NavigationContainer>
 
+const Stack = createStackNavigator()
+
+export default function App() {
+  const [expenses, setExpenses] = useState([])
+  
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name = "Home" component={HomeScreen} />
+        <Stack.Screen name = "Expense Details" component={ExpenseDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
